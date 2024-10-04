8bca4855b925e18cf54657f484fe2bfe05306e06
Paresh <pareshkulkarni9@gmail.com>
2024-10-04 18:16:09 +0530

    first commit

---
+++ playintegritylibrary/src/main/java/com/example/playintegritylibrary/PlayIntegrityClass.java
@@ -0,0 +1,7 @@
+package com.example.playintegritylibrary;
+
+public class PlayIntegrityClass {
+    public void displayName(String name){
+        System.out.println("Playintegrtiy" + name);
+    }
+}
